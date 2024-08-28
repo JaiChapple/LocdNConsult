9e0acf1ffa8e509844446216784edf99586ca8b3
Jai Chapple <65983283+JaiChapple@users.noreply.github.com>
2024-08-28 12:53:37 -0500

    Update page.tsx

--- lacodn-studio-v2/app/page.tsx
+++ lacodn-studio-v2/app/page.tsx
@@ -1,4 +1,11 @@
+import HomePageForm from "@/components/home"
+
 export default function Home() {
-    return <h1>Test Home Page</h1>;
+    return (
+        <div className="space-y-6 sm:px-10 sm:py-6 px-4 py-3">
+            <h1 className="sm:text-4xl text-2xl font-bold text-center">{"Loc'd"} N Studios</h1>
+            <HomePageForm />
+        </div>
+    )
 }
 
